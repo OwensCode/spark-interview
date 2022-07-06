59e3180479c7c2b157cda04ef03da09c608ac71c
Owen Berry <owen.berry@bounteous.com>
2022-07-06 17:18:29 -0400

    Updates to Java code

--- src/main/java/monme/RunMeJava.java
+++ src/main/java/monme/RunMeJava.java
@@ -5,7 +5,12 @@ import org.apache.spark.sql.SparkSession;
 
 public class RunMeJava {
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setAppName("MonMe").setMaster("local");
+        SparkConf conf = new SparkConf()
+                .setAppName("MonMe")
+                .setMaster("local")
+                .set("spark.driver.host", "localhost")
+                .set("spark.ui.enabled", "false");
+        
         SparkSession spark = SparkSession.builder().config(conf).getOrCreate();
 
         System.out.println("Hello World!");
